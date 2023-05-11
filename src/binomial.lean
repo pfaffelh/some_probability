6ef008500a84f4278df4266beb767af33e437b28
pfaffelh <p.p@stochastik.uni-freiburg.de>
2023-05-11 23:26:27 +0200

    initial commit

--- src/binomial.lean
+++ src/binomial.lean
@@ -6,29 +6,56 @@ import data.real.basic
 import probability.probability_mass_function.constructions
 
 noncomputable theory
-open_locale classical big_operators nnreal 
-
--- We need that x : ℝ≥0 is defined to be ⟨ x, hx ⟩ with x : ℝ and hx : x ≥ 0.
--- This is a lemma on ℝ≥0 we will need below. 
-
-variables {α : Type*}
-lemma nnreal_sum_eq_one_iff [fintype α] (f : α → ℝ ) (pos : ∀ a, f a ≥ 0) : ∑ a, f a = 1 ↔ ∑ a, (⟨ f a, pos a ⟩ : ℝ≥0) = 1 :=
-begin
-  rw ← nnreal.eq_iff, 
-  rw nnreal.coe_sum,
-  refl,
-end
-
--- We extend the API for defining a pmf from some f : α → ℝ (not → ℝ≥0); see probability_mass_function in mathlib for some more lemmas of the same type:
-
-def local_of_fintype [fintype α] (f : α → ℝ) (pos : ∀ a : α, 0 ≤ f a ) (h : ∑ a, f a = 1) : pmf α :=
-pmf.of_finset (λ a, ⟨ f a, pos a ⟩) finset.univ ((nnreal_sum_eq_one_iff f pos).1 h) (λ a ha, absurd (finset.mem_univ a) ha)
+open_locale classical big_operators nnreal ennreal 
 
 -- We are going to define B(n,p), the binomial distribution with n trials and success probability p.
 
+-- These are two basic lemmas on ℝ≥0∞ we will need below. 
+
+lemma eq_iff {a b : ℝ} (posb : 0 < b): ((a = b) ↔ (ennreal.of_real a = ennreal.of_real b)) :=
+begin
+  split, 
+  intro h, 
+  rw h, 
+  intro h, 
+  have hb : b = (ennreal.of_real b).to_real, 
+  {
+    have h : 0 ≤ b, apply le_of_lt posb, 
+    exact left_eq_sup.mpr h,  
+  },
+  rw hb, rw ← h,
+  have ha : 0 < ennreal.of_real a, 
+  {
+    rw h, simp only [ennreal.of_real_pos, gt_iff_lt], exact posb, 
+  },
+  have posa : 0 < a, 
+  { 
+    apply ennreal.of_real_pos.1 ha, },
+  have h : 0 ≤ a, 
+  { 
+    apply le_of_lt posa, },
+  exact left_eq_sup.mpr h,  
+end
+
+variables {α : Type*}
+
+lemma ennreal_sum_eq_one_iff [fintype α] (f : α → ℝ) (pos : ∀ a, f a ≥ 0) : (∑ a, f a = 1) ↔ (∑ a, (ennreal.of_real (f a)) = (1 : ℝ≥0∞)) :=
+begin
+  rw ← ennreal.of_real_sum_of_nonneg, 
+  rw eq_iff zero_lt_one, rw ennreal.of_real_one, 
+  intros a ha, exact pos a, 
+end
+
+-- We extend the API for defining a pmf from some f : α → ℝ (not → ℝ≥0∞); see probability_mass_function in mathlib for some more lemmas of the same type:
+
+def local_of_fintype [fintype α] (f : α → ℝ) (pos : ∀ a : α, 0 ≤ f a ) (h : ∑ a, f a = 1) : pmf α :=
+pmf.of_finset (λ a, ennreal.of_real (f a)) finset.univ ((ennreal_sum_eq_one_iff f pos).1 h) (λ a ha, absurd (finset.mem_univ a) ha)
+
+-- Here we start to define B(n,p)
+
 variables (n : ℕ) (p : ℝ)  
 
--- We start by defining mass function (mf) with codomain ℝ. A mf is a pmf (bit codomain ℝ) lacking a proof of summing to 1.
+-- We start by defining mf_binom, which is a mass function with codomain ℝ, but lacking a proof of summing to 1.
 
 def mf_binom (n : ℕ) (p : ℝ) : finset.range (n+1) → ℝ := λ a, ((n.choose a) : ℝ) * (p ^ (a : ℕ) )  * (1 - p) ^ (n - a)
 
@@ -38,7 +65,7 @@ begin
   refl, 
 end
 
--- Since a pmf has codomain ℝ≥0, we need the following:
+-- Since a pmf has codomain ℝ≥0∞, we need the following:
 lemma mf_binom_pos (n : ℕ) (p : ℝ) (hp1 : p ≥ 0) (hp2 : p ≤ 1) : ∀ (a : finset.range (n+1)), (mf_binom n p) a ≥ 0 := 
 begin
   intro a, 
@@ -76,7 +103,6 @@ begin
   apply finset.sum_subtype,
   intro x, refl,
 end
-
 
 -- Now, define the pmf for the binomial distribution
 def pmf_binom (n : ℕ) (p : ℝ) (hp1 : 0 ≤ p) (hp2 : p ≤ 1) : pmf (finset.range (n+1)) := local_of_fintype  (mf_binom n p) (mf_binom_pos n p hp1 hp2) (mf_binom_sum_one n p) 
